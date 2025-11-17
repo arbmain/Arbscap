959d1c91667e644d641d4ab0bba1346dc7753ff2
arbmain <smarts9tradingltd1@gmail.com>
2025-11-18 00:42:20 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    turbo: false, // disables Turbopack
+  },
   webpack(config) {
     config.resolve.alias['@'] = path.resolve(__dirname);
     return config;
